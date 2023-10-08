b6fbfca48524fed230d98a55b33d0b8db0b47df8
Nafis Abdullah Ahmad <n.ahmad.web.cit22@gmail.com>
2023-10-08 16:00:48 +0600

    pixel tag added

--- resources/views/pages/layouts/master.blade.php
+++ resources/views/pages/layouts/master.blade.php
@@ -42,6 +42,32 @@
     <meta name="facebook-domain-verification" content="lakjzs0vhg73078a1sirl7arc7mvux"  />
     <!--facebook-domain-verification-->
 
+    <!-- Meta Pixel Code -->
+    <script>
+        ! function(f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function() {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window, document, 'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '6814850821967961');
+        fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=6814850821967961&ev=PageView&noscript=1" /></noscript>
+    <!-- End Meta Pixel Code -->
+
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>@yield('title', $settings->name)</title>
