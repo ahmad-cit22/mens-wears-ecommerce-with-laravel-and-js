c16354c8075ebfe5b9935f805ed917ddb3af97d2
Nafis Abdullah Ahmad <n.ahmad.web.cit22@gmail.com>
2023-11-13 13:00:17 +0600

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -31,7 +31,7 @@ class HomeController extends Controller {
             $orders_not_final = Order::all();
             $yearly_orders = Order::where('is_final', 1)->whereYear('created_at', Carbon::now()->year)->get();
             $monthly_orders = Order::where('is_final', 1)->whereYear('created_at', Carbon::now()->year)->whereMonth('created_at', Carbon::now()->month)
-                ->get();
+                ->get(); 
             $daily_orders = Order::where('is_final', 1)->whereDate('created_at', Carbon::today())->get();
             return view('admin.index', compact('orders', 'orders_not_final', 'yearly_orders', 'monthly_orders', 'daily_orders'));
         } else if (Auth::user()->type == 2) {
